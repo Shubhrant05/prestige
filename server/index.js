4e29f5109ca154dd53eca7fe79fa0e53c975d3c3
Shubhrant05 <shubhrantyadav34366@gmail.com>
2024-04-14 20:51:14 +0530

    connected to MONGO_DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config() //used to load environment variables from a .env file into process.env
 
 const app = express();
 const PORT = 4000;
+
+//Connecting to MONGODB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('DB connected')
+}).catch((err) => {
+    console.log('DB connection error', err)
+})
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
