6373798e10ef37ea66c0e980bc55d13be79db6d4
Shubhrant Yadav <shubhrantyadav34366@gmail.com>
2024-05-11 17:26:40 +0530

    created update listing API route

--- server/controllers/listing-controller.js
+++ server/controllers/listing-controller.js
@@ -1,7 +1,7 @@
 import Listing from "../models/listing-model.js";
 
 export const createListing = async (req, res) => {
-    try{
+    try {
         const newListing = await Listing.create(req.body);
         res.status(201).json(newListing);
     } catch (error) {
@@ -12,19 +12,41 @@ export const createListing = async (req, res) => {
 export const deleteListing = async (req, res, next) => {
 
     const listing = await Listing.findById(req.params.id);
-  
+
     if (!listing) {
-      return res.status(404).json('Listing not found!');
+        return res.status(404).json('Listing not found!');
     }
-  
+
     if (req.user.id !== listing.userRef) {
-      return res.status(403).json('You can delete only your listings!');
+        return res.status(403).json('You can delete only your listings!');
     }
-  
+
     try {
-      await Listing.findByIdAndDelete(req.params.id);
-      res.status(200).json('Listing has been deleted!');
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted!');
     } catch (error) {
-      res.status(500).json("[SERVER ERROR] : Error while deleting listing " + error);
+        res.status(500).json("[SERVER ERROR] : Error while deleting listing " + error);
     }
-  };
+};
+
+export const updateListing = async (req, res) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return res.status(404).json('Listing not found!');
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return res.status(403).json('You can update only your listings!');
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true });
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        res.status(500).json("[SERVER ERROR] : Error while updating listing " + error);
+    }
+}
--- server/routes/listing-routes.js
+++ server/routes/listing-routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createListing, deleteListing } from '../controllers/listing-controller.js';
+import { createListing, deleteListing, updateListing} from '../controllers/listing-controller.js';
 import { verifyUser } from '../../verifyUser.js';
 const router = express.Router()
 
 router.post('/create', verifyUser, createListing)
 router.delete('/delete/:id', verifyUser, deleteListing) 
+router.post('/update/:id', verifyUser, createListing)
 export default router;
